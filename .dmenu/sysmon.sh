e0e3ca27fd76ab6119ec201e1771598a0ef373bb
Terke <anastasiostrk@protonmail.com>
2020-03-15 14:40:06 +0200

    Update .dmenu

---
+++ .dmenu/sysmon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Dmenu script for launching system monitoring programs.
+
+declare -a options=("htop
+glances
+gtop
+iftop
+iotop
+iptraf-ng
+quit")
+
+choice=$(echo -e "${options[@]}" | dmenu -l -i -p 'System monitors: ')
+
+case $choice in
+	quit)
+		echo "Program terminated." && exit 1
+	;;
+	htop| \
+	glances| \
+	gtop| \
+	iftop| \
+	iotop| \
+	iptraf-ng)
+        exec st -e gksu $choice
+	;;
+	*)
+		exit 1
+	;;
+esac
+
